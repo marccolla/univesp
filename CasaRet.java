4f1f5eb66863dbf1e55492c2fa9a96f4d14defb9
marcolla <marcolla.sv@gmail.com>
2019-05-25 11:14:15 -0300

    aulas06 v2

--- CasaRet.java
+++ CasaRet.java
@@ -3,16 +3,17 @@ class CasaRet
 
 	public static void main(String[] args)
 	{
+		int lateral = 10; // comprimento lateral da cabana
+		int cquarto = 7;  // comprimento da lateral maior do quarto
 		int areaq; // area do quartos
 		int areas; // ara da sala
 		int areat; // area total
 
 		System.out.println("Programa para Calculo\nda area da casa");
-		areas = 10*10;
+		areas = lateral*lateral;
 		System.out.println("A area da Sala é " + areas);
-		areaq = 7*5;
+		areaq = cquarto*(lateral/2);
 		System.out.println("A area do Quarto é " + areaq);
-		areaq = 7*5;
 		System.out.println("A area do Banheiro é " + areaq);
 		areat = areas + 2*areaq;
 		System.out.println("A area Total é " + areat);	
