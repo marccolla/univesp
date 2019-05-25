711ba1341acf2016d8bdf5cffa3b926b5257d054
marcolla <marcolla.sv@gmail.com>
2019-05-25 00:56:00 -0300

    aula06

---
+++ CasaRet.java
@@ -0,0 +1,23 @@
+class CasaRet
+{
+
+	public static void main(String[] args)
+	{
+		int areaq; // area do quartos
+		int areas; // ara da sala
+		int areat; // area total
+
+		System.out.println("Programa para Calculo\nda area da casa");
+		areas = 10*10;
+		System.out.println("A area da Sala é " + areas);
+		areaq = 7*5;
+		System.out.println("A area do Quarto é " + areaq);
+		areaq = 7*5;
+		System.out.println("A area do Banheiro é " + areaq);
+		areat = areas + 2*areaq;
+		System.out.println("A area Total é " + areat);	
+
+
+	}
+
+}
