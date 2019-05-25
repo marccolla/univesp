71ddcc24842e7384d8258fb3b0f871a4bef1aabf
marcolla <marcolla.sv@gmail.com>
2019-05-25 00:24:11 -0300

    teste

---
+++ AreaPiscina.java
@@ -0,0 +1,22 @@
+class AreaPiscina
+{
+
+	public static void main(String[] args)
+	{
+		// raio piscina
+		double raio = 2;
+		// area da piscina 
+		double areap;
+		// valor de pi
+		final double pi = 3.14159;
+
+		
+
+		areap = pi * raio * raio;
+
+		System.out.println("A area  " + areap);	
+
+
+	}
+
+}
